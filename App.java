81044656e617e676384ad5dc6c33a28be63df1d6
可期 <keke.zkk@alibaba-inc.com>
2019-08-08 10:36:53 +0800

    dsfasd asdf asdfdsf

--- App.java
+++ App.java
@@ -1,13 +1,42 @@
-package com.mycompany.app;
+package io.rsocket;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.concurrent.CountDownLatch;
+import org.openjdk.jmh.infra.Blackhole;
+import org.reactivestreams.Subscription;
+import reactor.core.CoreSubscriber;
+
+public class PerfSubscriber implements CoreSubscriber<Payload> {
+
+  final CountDownLatch latch = new CountDownLatch(1);
+  final Blackhole blackhole;
+
+  Subscription s;
+
+  public PerfSubscriber(Blackhole blackhole) {
+    this.blackhole = blackhole;
+  }
+
+  @Override
+  public void onSubscribe(Subscription s) {
+    this.s = s;
+    s.request(1);
+  }
+
+  @Override
+  public void onNext(Payload payload) {
+    payload.release();
+    blackhole.consume(payload);
+    s.request(1);
+  }
+
+  @Override
+  public void onError(Throwable t) {
+    blackhole.consume(t);
+    latch.countDown();
+  }
+
+  @Override
+  public void onComplete() {
+    latch.countDown();
+  }
 }
